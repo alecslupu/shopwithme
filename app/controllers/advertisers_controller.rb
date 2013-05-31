efc5f5b25432f79c0500a265c535c95f24c5292a
alecslupu <contact@alecslupu.ro>
2013-05-31 20:26:39 +0300

    fixed the brand advertiser fuck-up

--- app/controllers/advertisers_controller.rb
+++ app/controllers/advertisers_controller.rb
@@ -1,6 +1,8 @@
 class AdvertisersController < ApplicationController
+  before_filter :fix_brand_advertiser, :only => [ :show ]
+
+
   def show
-    @advertiser = Advertiser.find(params[:id])
     product_count = Rails.cache.fetch("all_advertiser_#{@advertiser.id}_products_count",:expires_in => 6.hours) { @advertiser.products.size  }
     @products = @advertiser.products.includes(:category, :advertiser).page_with_cached_total_count(params[:page], product_count)
   end
@@ -8,4 +10,13 @@ class AdvertisersController < ApplicationController
   def index
     @advertisers = Advertiser.alphabetically.with_products.page(params[:page])
   end
+
+  private 
+  def fix_brand_advertiser
+    @advertiser = Advertiser.where(:slug => params[:id]).first
+    if @advertiser.nil?
+      brand = Brand.where(:slug => params[:id]).first
+      redirect_to brand_path(brand), :status => :moved_permanently unless brand.nil?
+    end
+  end 
 end
