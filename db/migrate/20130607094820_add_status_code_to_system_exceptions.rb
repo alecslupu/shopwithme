d41bc204b162631701abcba176527b276da0fcf5
alecslupu <contact@alecslupu.ro>
2013-06-07 23:04:15 +0300

    migration fix

--- db/migrate/20130607094820_add_status_code_to_system_exceptions.rb
+++ db/migrate/20130607094820_add_status_code_to_system_exceptions.rb
@@ -1,7 +1,5 @@
 class AddStatusCodeToSystemExceptions < ActiveRecord::Migration
   def change
-    remove_column :system_exceptions, :uri
-
     add_column :system_exceptions, :status_code, :string
   end
 end
