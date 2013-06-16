749f02a0b5e70d63925d057808a4dd4ecb0167e2
alecslupu <contact@alecslupu.ro>
2013-06-16 18:47:13 +0300

    fix ?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ ShopWithMe::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  config.action_controller.page_cache_directory = "#{Rails.root}/public/cache"
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
