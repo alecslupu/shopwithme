5ef63cf492d54e0ea7231d24219c47eca5075636
alecslupu <contact@alecslupu.ro>
2013-07-06 12:47:38 +0300

    domain file update

--- config/initializers/domain.rb
+++ config/initializers/domain.rb
@@ -1,5 +1,5 @@
 DOMAIN_NAME = 'ProductRepository'
 DOMAIN_STRAPLINE = "Product Database"
-ANALITYCS = 'UA-41086843-1'
+ANALITYCS = 'UA-9460563-1'
 DOMAIN_NAME_IN_WORDS = 'Product Repository'
 DOMAIN_BASE_URL = 'productrepository.com'
