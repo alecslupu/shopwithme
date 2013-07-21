a44e46dd47dc8da1171a94baba25bba9c0648e5c
alecslupu <contact@alecslupu.ro>
2013-07-21 11:01:53 +0300

    cookie droping

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,9 @@
 class ProductsController < ApplicationController
-  caches_page :show 
-  
+  # caches_page :show 
   before_filter :find_product, :only => [:show, :visit]
+  before_filter :redirect_to_product_view, :only => [ :show ]
+
+
   def index
     if bot?
       render :text => "", :status => :gone and return 
@@ -16,8 +18,6 @@ class ProductsController < ApplicationController
       if request.path != product_path(@product)
         redirect_to @product, status: :moved_permanently and return 
       end
-
-      log_product_view(@product)
     end
   end 
 
@@ -31,6 +31,12 @@ class ProductsController < ApplicationController
 
   private 
 
+  def redirect_to_product_view
+    if current_admin.nil?  and not bot? 
+      redirect_to visit_product_path(@product) and return 
+    end
+  end 
+
   def find_product
     @product = Product.cached_find(params[:id])
   end
@@ -42,15 +48,6 @@ class ProductsController < ApplicationController
   def skip_log
     return ( not request.referer.nil? and request.referer.start_with?(root_url) and not bot? and not current_admin )
   end
-
-  def log_product_view(product) 
-    product.display_logs.create({
-      :referrer => request.referer, 
-      :user_agent => request.user_agent, 
-      :ip => request.remote_ip
-    }) unless skip_log 
-
-  end 
 
   def log_product_visit(product)
     product.visit_logs.create({
